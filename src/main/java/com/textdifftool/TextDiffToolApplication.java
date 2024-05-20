f4672403c86e0858f6fa037ed8f420d748344bfb
krkarma777 <krkarma777@gmail.com>
2024-05-20 22:57:02 +0900

    Initial commit

---
+++ src/main/java/com/textdifftool/TextDiffToolApplication.java
@@ -0,0 +1,13 @@
+package com.textdifftool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextDiffToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextDiffToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/textdifftool/TextDiffToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.textdifftool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextDiffToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
