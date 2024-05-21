00224b51bcec3a661f2279ca09769ed66e594c44
krkarma777 <krkarma777@gmail.com>
2024-05-21 12:26:25 +0900

    Rename all 'home' references to 'diff'

--- src/main/java/com/textdifftool/web/controller/DiffController.java
+++ src/main/java/com/textdifftool/web/controller/DiffController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ViewController {
+public class DiffController {
 
     @GetMapping
-    public String home() {
-        return "home";
+    public String diff() {
+        return "diff";
     }
 }
